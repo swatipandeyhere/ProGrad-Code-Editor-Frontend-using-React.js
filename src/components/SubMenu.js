5e183528a05b54238107b838192da66835a05433
Swati Pandey <swatipandeyhere@gmail.com>
2024-05-17 18:26:32 +0530

    Create Sub Menu

---
+++ src/components/SubMenu.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./SubMenu.css"
+import Reload from '../images/Reload.png'
+
+export default function SubMenu() {
+    return (
+        <div className="sub-menu">
+            <span>Description</span>
+            <a href="/">
+                <span>Solutions</span>
+            </a>
+
+            <select name="languages" id="languages">
+                <option value="Select Language">Select Language</option>
+                <option value="CPP">CPP</option>
+                <option value="Java">Java</option>
+                <option value="Python">Python</option>
+                <option value="C">C</option>
+            </select>
+
+            <a href="/">
+                <img src={Reload} alt="Reload" />
+            </a>
+        </div>
+    )
+}
